db77161841a6607400ae4bf9c18a416e6a8c70bb
Madebybrown <madebybrown@gmail.com>
2022-11-24 15:59:45 +0000

    Update task list class object name, add detail class view, set template name, connect urls

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, DetailTask
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', DetailTask.as_view(), name='task'),
 ]
