4982890d997576fe834577dc42bc67c114167fbc
Madebybrown <madebybrown@gmail.com>
2022-11-27 07:57:30 +0000

    Add create task class view

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 
@@ -14,3 +15,9 @@ class DetailTask(DetailView):
     model = Task
     template_name = 'base/task_detail.html'
     context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'base/task_form.html'
+    field = '__all__'
