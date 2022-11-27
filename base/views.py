42da41148ab204781aea221e391f7687c80ba58d
Madebybrown <madebybrown@gmail.com>
2022-11-27 11:10:51 +0000

    Restrict user from creating items globally

--- base/views.py
+++ base/views.py
@@ -41,13 +41,17 @@ class DetailTask(LoginRequiredMixin, DetailView):
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'base/task_form.html'
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
