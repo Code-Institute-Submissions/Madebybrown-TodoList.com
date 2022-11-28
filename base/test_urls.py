0a1f01e4507cdb255110afe9505a6dbf7ba1aca5
Madebybrown <madebybrown@gmail.com>
2022-11-28 19:02:05 +0000

    Test register page url

--- base/test_urls.py
+++ base/test_urls.py
@@ -1,10 +1,14 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from . import views
+from base.views import CustomLoginView, RegisterPage, TaskList, DetailTask, TaskCreate, TaskUpdate, DeleteView
 
 
 class TestUrls(SimpleTestCase):
 
     def test_login_url_is_resolved(self):
         url = reverse('login')
-        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, CustomLoginView)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func.view_class, RegisterPage)
