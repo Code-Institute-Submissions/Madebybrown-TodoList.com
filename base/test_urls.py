1dde80fa17c1bb5f939dbcad131f8105d618482b
Madebybrown <madebybrown@gmail.com>
2022-11-28 19:11:55 +0000

    Test TaskCreate page url

--- base/test_urls.py
+++ base/test_urls.py
@@ -16,3 +16,7 @@ class TestUrls(SimpleTestCase):
     def test_tasks_url_is_resolved(self):
         url = reverse('tasks')
         self.assertEquals(resolve(url).func.view_class, TaskList)
+
+    def test_task_create_url_is_resolved(self):
+        url = reverse('task-create')
+        self.assertEquals(resolve(url).func.view_class, TaskCreate)
