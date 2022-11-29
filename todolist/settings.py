df86ff433ddc14af769cb90994d3078d5c30e42e
Madebybrown <madebybrown@gmail.com>
2022-11-29 15:07:55 +0000

    Uppdate insalled apps

--- todolist/settings.py
+++ todolist/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
     'base',
-    'todolist',
 ]
 
 MIDDLEWARE = [
