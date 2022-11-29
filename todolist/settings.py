30214be44071d250e093fb7987aab972589754d8
Madebybrown <madebybrown@gmail.com>
2022-11-29 17:39:39 +0000

    Add inline style

--- todolist/settings.py
+++ todolist/settings.py
@@ -127,7 +127,9 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
